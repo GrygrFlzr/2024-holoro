c5b30f67fe6343d74b26e9c67d683813a3da8edd
GrygrFlzr <grygrflzr@hotmail.com>
2024-11-27 15:40:33 +0700

    Initial single sticker implementation

--- src/routes/[talent]/2024-anniversary/[lang]/+page.svelte
+++ src/routes/[talent]/2024-anniversary/[lang]/+page.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import Piece from '$lib/Piece.svelte';
+	import { hashStringToBigInt, pcg64 } from '$lib/pcg';
 
 	let { data }: { data: PageData } = $props();
+	const numericSeed = hashStringToBigInt(data.seed);
+	const rng = pcg64(numericSeed);
+	const messageCount = data.messages.length;
+	const firstStickerPosition = rng.getRandomInt(0, messageCount - 1);
+	const authorExtractor = /\/(?:ollie|anya|reine)_(.*).png/;
 </script>
 
 <div class="page" style="
 	background-color: {data.bgColor};
 ">
-	{#each data.rows as piece, index}
+	{#each data.messages as piece, index}
+		{#if index == firstStickerPosition && data.stickers.length > 0}
+			<div class="sticker">
+				<!-- svelte-ignore a11y_missing_attribute -->
+				<img src={data.stickers[0]} />
+				<span data-name={authorExtractor.exec(data.stickers[0])![1]} class="author">{authorExtractor.exec(data.stickers[0])![1]}</span>
+			</div>
+		{/if}
 		<Piece {piece} {index} />
 	{/each}
 </div>
@@ -20,5 +33,30 @@
 		justify-content: space-around;
 		align-content: flex-start;
 		min-height: 100vh;
+	}
+	.sticker {
+		position: relative;
+	}
+	.sticker img {
+		max-width: 150px;
+	}
+	.sticker .author {
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		font-weight: 500;
+		color: white;
+		-webkit-text-stroke: 4px white;
+		transform: rotate(-5deg);
+	}
+	.sticker .author:after {
+		content: attr(data-name);
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		color: black;
+		-webkit-text-stroke: initial;
+		pointer-events: none;
 	}
 </style>
--- src/routes/[talent]/2024-anniversary/[lang]/+page.ts
+++ src/routes/[talent]/2024-anniversary/[lang]/+page.ts
@@ -1,5 +1,8 @@
 import type { PageLoad } from './$types';
 
+import ollieSticker01 from '$lib/stickers/ollie_deer_oh_dear.png';
+import reineSticker01 from '$lib/stickers/reine_alterzero.png'
+
 // false during open submission phase
 // set to true after removing edit permissions from sheet
 // to optimize out the web requests
@@ -8,43 +11,66 @@ export const ssr = false;
 const talentLangMap: Record<
 	string,
 	{
+		seed: string;
 		dataSource: Record<string, string>;
 		bgColor: string;
+		stickers: string[];
 	}
 > = {
 	ollie: {
+		seed: 'Kureiji Ollie',
 		dataSource: {
 			en: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSKpFpQgTdUptUeeL2k-dOK5jmmN72uLA543WloBkkhgsXBIGyly8aUKC0PJXZJEXjL39LOtVy7ksBv/pub?gid=0&single=true&output=tsv',
 			id: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSKpFpQgTdUptUeeL2k-dOK5jmmN72uLA543WloBkkhgsXBIGyly8aUKC0PJXZJEXjL39LOtVy7ksBv/pub?gid=2036774295&single=true&output=tsv'
 		},
 		bgColor: '#7f1d1d',
+		stickers: [
+			ollieSticker01
+		],
 	},
 	anya: {
+		seed: 'Anya Melfissa',
 		dataSource: {
 			en: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSlzbGq5R5EgSM93G84_0NYkWIpTRhGN4ZwFLzjwqa6LBRk4X0mx7GTFAfd5R8cxc3OClPWC99dqzdk/pub?gid=0&single=true&output=tsv',
 			id: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSlzbGq5R5EgSM93G84_0NYkWIpTRhGN4ZwFLzjwqa6LBRk4X0mx7GTFAfd5R8cxc3OClPWC99dqzdk/pub?gid=1966963793&single=true&output=tsv'
 		},
 		bgColor: '#78350f',
+		stickers: [
+		],
 	},
 	reine: {
+		seed: 'Pavolia Reine',
 		dataSource: {
 			en: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQKwyjpaD_KzJCYsEAlTM8MV54Rbpi8BCqY5ZJ2nEu-n735IArb-SOoAV-K03okERaHmusVbzYvaW6k/pub?gid=0&single=true&output=tsv',
 			id: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQKwyjpaD_KzJCYsEAlTM8MV54Rbpi8BCqY5ZJ2nEu-n735IArb-SOoAV-K03okERaHmusVbzYvaW6k/pub?gid=2036774295&single=true&output=tsv'
 		},
 		bgColor: '#1e3a8a',
+		stickers: [
+			reineSticker01
+		],
 	}
 };
 
 export const load: PageLoad = async ({ fetch, params }) => {
-	const { bgColor, dataSource } = talentLangMap[params.talent];
+	const {
+		bgColor,
+		stickers,
+		seed,
+		dataSource
+	} = talentLangMap[params.talent];
 	const res = await fetch(dataSource[params.lang]);
 	const raw_tsv = await res.text();
-	const rows = raw_tsv
+	const messages = raw_tsv
 		.split('\n')
 		.map((row) => {
 			const [author, message] = row.trim().split('\t');
 			return { author, message };
 		})
 		.slice(1);
-	return { rows, bgColor };
+	return {
+		messages,
+		stickers,
+		seed,
+		bgColor,
+	};
 };
